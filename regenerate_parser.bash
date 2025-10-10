12f1a7b06e998047d2deff633d03c663cd1253d3
Troy Wooton <gcowootont@gmail.com>
2025-10-09 16:38:07 -1000

    Fixed output directory

--- regenerate_parser.bash
+++ regenerate_parser.bash
@@ -4,6 +4,6 @@
 cd src/antlr4
 
 # First regenerate the lexer
-antlr4 CoolLexer.g4 -o ../java/grammar
+antlr4 CoolLexer.g4 -o ../main/java/grammar
 # Then regenerate the parser
-antlr4 CoolParser.g4 -lib ../java/grammar -o ../java/grammar -no-listener -visitor
+antlr4 CoolParser.g4 -lib ../main/java/grammar -o ../main/java/grammar -no-listener -visitor
